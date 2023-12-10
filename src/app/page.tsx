1b2584995270bf1278eb90fcc98a938f9295ff3e
Marvin Chew <marvin@marvincyk.com>
2023-12-11 01:29:59 +1100

    Encapsulate Board component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ export default function App() {
 	return (
 		<div className="flex h-screen items-center justify-around">
 			<Player name="X" />
-			<div className="grid grid-cols-3 gap-8 border rounded-xl shadow-[0_0_2rem__1rem_white] p-8">
-				{Array.from({ length: 9 }).map((_, index) => (
-					<Board key={index} />
-				))}
-			</div>
+			<Board />
 			<Player name="O" />
 		</div>
 	);
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-export default function Board() {
+function SubBoard() {
 	return (
 		<div className="grid grid-cols-3 gap-2">
 			{Array.from({ length: 9 }).map((_, index) => (
@@ -6,6 +6,16 @@ export default function Board() {
 					key={index}
 					className="bg-slate-900 hover:bg-slate-700 p-4 cursor-pointer"
 				></div>
+			))}
+		</div>
+	);
+}
+
+export default function Board() {
+	return (
+		<div className="grid grid-cols-3 gap-8 border rounded-xl shadow-[0_0_2rem__1rem_white] p-8">
+			{Array.from({ length: 9 }).map((_, index) => (
+				<SubBoard key={index} />
 			))}
 		</div>
 	);
