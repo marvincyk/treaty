6b327d692e419afba3854d035518399514c71361
Marvin Chew <marvin@marvincyk.com>
2023-12-27 22:40:29 +0700

    Hardcode board state

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,35 @@
-function SubBoard() {
+import { FaO, FaX } from "react-icons/fa6";
+
+const STATE = [
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["X", "O", "O", "", "X", "", "", "", "X"],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+];
+
+const renderCell = (parentIndex: number, index: number) => {
+	switch (STATE[parentIndex][index]) {
+		case "X":
+			return <FaX size="2rem" />;
+		case "O":
+			return <FaO size="2rem" />;
+		default:
+			break;
+	}
+};
+
+function SubBoard({ parentIndex }: { parentIndex: number }) {
 	return (
 		<div className="grid grid-cols-3 gap-0.5">
 			{Array.from({ length: 9 }).map((_, index) => (
-				<div key={index} className="p-8 cursor-pointer bg-black"></div>
+				<div key={index} className="p-4 cursor-pointer bg-black min-h-[4rem]">
+					{renderCell(parentIndex, index)}
+				</div>
 			))}
 		</div>
 	);
@@ -12,7 +39,7 @@ export default function Board() {
 	return (
 		<div className="grid grid-cols-3 gap-2 bg-white">
 			{Array.from({ length: 9 }).map((_, index) => (
-				<SubBoard key={index} />
+				<SubBoard key={index} parentIndex={index} />
 			))}
 		</div>
 	);
