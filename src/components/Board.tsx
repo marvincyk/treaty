96aefe8e4b473305cc9e88548ca4ed8505e7202c
Marvin Chew <marvin@marvincyk.com>
2024-01-08 05:59:39 +1100

    Move board state to `useState`

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,19 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import { FaO, FaX } from "react-icons/fa6";
 
-const STATE = [
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-	["X", "O", "O", "", "X", "", "", "", "X"],
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-	["", "", "", "", "", "", "", "", ""],
-];
-
-const renderCell = (parentIndex: number, index: number) => {
-	switch (STATE[parentIndex][index]) {
+const renderCell = (
+	boardState: string[][],
+	parentIndex: number,
+	index: number
+) => {
+	switch (boardState[parentIndex][index]) {
 		case "X":
 			return <FaX size="2rem" />;
 		case "O":
@@ -23,12 +18,18 @@ const renderCell = (parentIndex: number, index: number) => {
 	}
 };
 
-function SubBoard({ parentIndex }: { parentIndex: number }) {
+function SubBoard({
+	parentIndex,
+	boardState,
+}: {
+	parentIndex: number;
+	boardState: string[][];
+}) {
 	return (
 		<div className="grid grid-cols-3 gap-0.5">
 			{Array.from({ length: 9 }).map((_, index) => (
 				<div key={index} className="p-4 cursor-pointer bg-black min-h-[4rem]">
-					{renderCell(parentIndex, index)}
+					{renderCell(boardState, parentIndex, index)}
 				</div>
 			))}
 		</div>
@@ -36,10 +37,21 @@ function SubBoard({ parentIndex }: { parentIndex: number }) {
 }
 
 export default function Board() {
+	const [boardState, setBoardState] = useState([
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+		["X", "O", "O", "", "X", "", "", "", "X"],
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+		["", "", "", "", "", "", "", "", ""],
+	]);
 	return (
 		<div className="grid grid-cols-3 gap-2 bg-white">
 			{Array.from({ length: 9 }).map((_, index) => (
-				<SubBoard key={index} parentIndex={index} />
+				<SubBoard key={index} parentIndex={index} boardState={boardState} />
 			))}
 		</div>
 	);
